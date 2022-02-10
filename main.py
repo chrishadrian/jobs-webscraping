59f1f83bab98f8de3e6f9212f7a890c023c0a2ce
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2022-02-10 01:49:25 -0800

    Learned the basics of SQL

---
+++ main.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+conn = sqlite3.connect('lunch.db') #connect to database
+c = conn.cursor() # access to the database
+
+
+# make columns and data type
+#c.execute('''CREATE TABLE meals(sandwich TEXT, fruit TEXT, tablenumber INT)''')
+
+c.execute('''DROP TABLE meals''') ## delete table from database
+
+sandwich = 'jam'
+fruit = 'sandwich'
+tablenum = 23
+
+#c.execute('''INSERT INTO meals VALUES(?,?,?)''', (sandwich, fruit, tablenum))
+
+#conn.commit() # after insert don't forget to commit
+
+# c.execute('''SELECT * FROM meals''')
+c.execute('''SELECT fruit FROM meals''')
+results = c.fetchall()
+print(results)

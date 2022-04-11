3154b70125866b4131b3adf935c88cdef5ff13be
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2022-04-10 23:25:44 -0700

    Update database.py

--- database.py
+++ database.py
@@ -5,19 +5,20 @@ c = conn.cursor() # access to the database
 
 
 # make columns and data type
-#c.execute('''CREATE TABLE meals(sandwich TEXT, fruit TEXT, tablenumber INT)''')
+
 
 # c.execute('''DROP TABLE meals''') ## delete table from database
 
+c.execute('''CREATE TABLE meals(sandwich TEXT, fruit TEXT, tablenumber INT)''')
 sandwich = 'jam'
 fruit = 'sandwich'
 tablenum = 23
 
-#c.execute('''INSERT INTO meals VALUES(?,?,?)''', (sandwich, fruit, tablenum))
+c.execute('''INSERT INTO meals VALUES(?,?,?)''', (sandwich, fruit, tablenum))
 
-#conn.commit() # after insert don't forget to commit
+conn.commit() # after insert don't forget to commit
 
-# c.execute('''SELECT * FROM meals''')
-c.execute('''SELECT fruit FROM meals''')
+c.execute('''SELECT * FROM meals''')
+# c.execute('''SELECT fruit FROM meals''')
 results = c.fetchall()
 print(results)

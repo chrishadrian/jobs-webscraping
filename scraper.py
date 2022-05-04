b9d012b0ec11d3e8f25bb85a2119542ce8fe8f97
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2022-05-04 15:35:58 -0700

    Integrate storing to CSV and DB to main

--- scraper.py
+++ scraper.py
@@ -3,10 +3,6 @@ import sqlite3
 from bs4 import BeautifulSoup
 import pandas as pd
 
-conn = sqlite3.connect('jobs.db')
-c = conn.cursor()
-joblist = []
-
 def extract(page):
     headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36'}
     url = f"https://ca.indeed.com/jobs?q=software+engineer+intern&l=Vancouver%2C+BC&start={page}"
@@ -14,11 +10,10 @@ def extract(page):
     soup = BeautifulSoup(r.content, 'html.parser')
     return soup
 
-def storeDB(title, company, location, date, summary):
+def storeDB(title, company, location, date, summary, c):
     c.execute('''INSERT INTO jobs VALUES(?,?,?,?,?)''', (title, company, location, date, summary))
-    conn.commit()
 
-def storeCSV(title, company, location, date, summary):
+def storeCSV(title, company, location, date, summary, joblist):
     job = {
         'Title': title,
         'Company': company,
@@ -28,7 +23,7 @@ def storeCSV(title, company, location, date, summary):
         }
     joblist.append(job)
 
-def transform(soup):
+def transform(soup, c, joblist):
     divs = soup.find_all('div', class_ = 'job_seen_beacon')
     for item in divs:
         unwanted = item.find('span', class_ = 'visually-hidden')
@@ -39,28 +34,33 @@ def transform(soup):
         date = item.find('span', class_ = 'date').text.strip()
         summary = item.find('div', class_ = 'job-snippet').text.strip().replace('\n', '')
 
-        storeDB(title, company, location, date, summary)
-        storeCSV(title, company, location, date, summary)
+        storeDB(title, company, location, date, summary, c)
+        storeCSV(title, company, location, date, summary, joblist)
     return
 
 def main():
+    conn = sqlite3.connect('jobs.db')
+    c = conn.cursor()
+    joblist = []
+    
     try:
         c.execute('''DROP TABLE jobs''') ## delete table from database
-    finally:
+    except:
         c.execute('''CREATE TABLE jobs(Title TEXT, Company TEXT, Location TEXT, Date TEXT, Summary TEXT)''')
 
     # extract all pages
     for i in range(0, 40, 10):
         print(f'Getting page, {i}')
-        c = extract(i)
-        transform(c)
+        p = extract(i)
+        transform(p, c, joblist)
 
     # Saving data to csv file
     df = pd.DataFrame(joblist)
     df.to_csv('jobs.csv')
     # print(df.head())
 
-    # Select which category in database to display in the db file
+    # Saving data to db file
+    conn.commit()
     c.execute('''SELECT * FROM jobs''')
     results = c.fetchall()
     print(results)

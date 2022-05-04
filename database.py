30ee693cc10325afebe3d59128372c5d38bf6547
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2022-05-03 18:17:01 -0700

    Refactored variables names in database

--- database.py
+++ database.py
@@ -1,24 +1,26 @@
 import sqlite3
 
-conn = sqlite3.connect('lunch.db') #connect to database
+conn = sqlite3.connect('jobs.db') #connect to database
 c = conn.cursor() # access to the database
 
 
 # make columns and data type
 
 
-# c.execute('''DROP TABLE meals''') ## delete table from database
+# c.execute('''DROP TABLE jobs''') ## delete table from database
 
-c.execute('''CREATE TABLE meals(sandwich TEXT, fruit TEXT, tablenumber INT)''')
-sandwich = 'jam'
-fruit = 'sandwich'
-tablenum = 23
+# c.execute('''CREATE TABLE jobs(Title TEXT, Company TEXT, Location TEXT, Date TEXT, Summary TEXT)''')
+title = 'swe'
+company = 'sap'
+location = 'vancouver'
+date = '1 day ago'
+summary = 'yadayadayada'
 
-c.execute('''INSERT INTO meals VALUES(?,?,?)''', (sandwich, fruit, tablenum))
+c.execute('''INSERT INTO jobs VALUES(?,?,?,?,?)''', (title, company, location, date, summary))
 
-conn.commit() # after insert don't forget to commit
+# conn.commit() # after insert don't forget to commit
 
-c.execute('''SELECT * FROM meals''')
-# c.execute('''SELECT fruit FROM meals''')
+c.execute('''SELECT * FROM jobs''')
+# c.execute('''SELECT Title FROM jobs''')
 results = c.fetchall()
 print(results)
